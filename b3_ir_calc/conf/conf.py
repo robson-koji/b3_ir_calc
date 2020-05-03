aa53dd761468c85213c69a3d93b55f347b38d39e
bug fixer <robson.koji@gmail.com>
2020-05-03 20:11:44 -0300

    working with CEI data, and creating conf file

---
+++ b3_ir_calc/conf/conf.py
@@ -0,0 +1,16 @@
+
+
+
+""" CEI """
+# CEI input file
+CEI_INPUT_PATH = ''
+
+# CEI output file
+CEI_OUTPUT_PATH = ''
+
+
+""" Local configuration """
+try:
+    from local_conf import *
+except ImportError:
+    pass
